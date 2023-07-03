512c881e36afcc4e12cb4ba2fd13516f0431b5b0
Daniel <danielson@college.harvard.edu>
2023-07-02 22:52:43 -0400

    minesweeper v1

--- sketch.js
+++ sketch.js
@@ -66,7 +66,7 @@ function mousePressed() {
 
                 if (grid[i][j].bomb) {
                     gameOver();
-                    window.alert("LOSER!!!");
+                    alert("LOSER!!!");
                 }
             }
         }
